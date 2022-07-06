26e09ae500b21e1f04c1a92e093437d3f38d676a
hamyo <zabey_gvozd@list.ru>
2022-07-06 10:51:56 +0300

    appexception refactor

--- src/main/java/dbdiff/domain/AppException.java
+++ src/main/java/dbdiff/domain/AppException.java
@@ -4,4 +4,9 @@ public class AppException extends RuntimeException {
     public AppException(String message) {
         super(message);
     }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
